bc403104768d593ef1e7a51660c144b0615f3b99
Sivashish <84663262+Sivashish@users.noreply.github.com>
2023-03-10 13:04:45 +0530

    Add files via upload

---
+++ DynamicArraysClasses.java
@@ -0,0 +1,82 @@
+package oops_1_2_3.classes_and_object;
+
+public class DynamicArraysClasses {
+    private int[] arr;
+    private int nextIndex;
+
+    public DynamicArraysClasses(){
+        arr = new int[5];
+        nextIndex = 0;
+    }
+
+    public void add(int element){
+        if(nextIndex == arr.length){
+            reconstruct();
+        }
+        arr[nextIndex] = element;
+        nextIndex++;
+    }
+
+    public int size(){
+        return nextIndex;
+    }
+
+    public void set(int index, int element){
+        if(index > nextIndex){
+            return;
+        }
+        if(index < nextIndex){
+            arr[index] = element;
+        }else{
+            add(element);
+        }
+    }
+
+    public int get(int index) {
+        if(index >= nextIndex){
+            return -1;
+        }
+        return arr[index];
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public int removeLast(){
+        if (size() == 0){
+            return -1;
+        }
+        int val = arr[nextIndex - 1];
+        arr[nextIndex - 1] = 0;
+        nextIndex--;
+        return val;
+    }
+
+    private void reconstruct(){
+        int[] temp = arr;
+        arr = new int[arr.length * 2];
+        System.arraycopy(temp, 0, arr, 0, temp.length);
+    }
+
+    public void printArrays(){
+        if (size() == 0){
+            return;
+        }
+        for (int i = 0; i < nextIndex; i++) {
+            if(i == 0){
+                System.out.print("[");
+            }
+            if (i > 0) {
+                System.out.print(", ");
+            }
+
+            System.out.print(arr[i]);
+
+            if (i == nextIndex-1) {
+                System.out.print("]");
+            }
+        }
+        System.out.println();
+    }
+}
